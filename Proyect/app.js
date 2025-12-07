0c920f6c35aba41956ec8b58d70c51e4115d8f20
TasyDev <tamayocontat@gmail.com>
2025-12-07 05:34:59 -0500

    Add: edit and delete

--- Proyect/app.js
+++ Proyect/app.js
@@ -10,7 +10,20 @@ addTaks.addEventListener("submit", (event) => {
         <span class="delete-btn">❌</span>
         <span class="edit-btn">✏️</span>
     `;
-    console.log(taks.value)
     addMensage.appendChild(newTask)
 })
+
+addMensage.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-btn")) {
+        console.log(event.target.parentElement)
+        event.target.parentElement.remove();
+    }
+
+    if (event.target.classList.contains("edit-btn")) {
+        console.log(event.target.parentElement)
+        const newName = prompt()
+        event.target.parentElement.childNodes[0].textContent = newName
+    }
+})
+
 
