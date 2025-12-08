3f0004f62391f2be139dbfd0e90f046b0b4dbb72
TasyDev <tamayocontat@gmail.com>
2025-12-07 21:27:43 -0500

    Add: LocalStorage and finihs course dom

--- Proyect/app.js
+++ Proyect/app.js
@@ -11,19 +11,56 @@ addTaks.addEventListener("submit", (event) => {
         <span class="edit-btn">✏️</span>
     `;
     addMensage.appendChild(newTask)
+    let newId = localStorage.length
+    toLocal(newId,taks.value)
 })
 
 addMensage.addEventListener("click", (event) => {
     if (event.target.classList.contains("delete-btn")) {
-        console.log(event.target.parentElement)
-        event.target.parentElement.remove();
+        const li = event.target.parentElement;
+        const key = li.dataset.key; // 👈 aquí tienes tu key
+
+        localStorage.removeItem(key);
+        li.remove();
     }
 
     if (event.target.classList.contains("edit-btn")) {
         console.log(event.target.parentElement)
         const newName = prompt()
+        const li = event.target.parentElement
+        const key = li.dataset.key
+        localStorage.setItem(key,newName)
         event.target.parentElement.childNodes[0].textContent = newName
     }
+
 })
+
+function toLocal(i,data){
+    localStorage.setItem(i, data);
+}
+
+function localLoad(){
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let data = localStorage.getItem(key);
+
+        const loadTaks = document.createElement("li")
+        loadTaks.dataset.key = key; // 👈 guardamos la key en el li
+
+        loadTaks.innerHTML = `
+            ${data}
+            <span class="delete-btn">❌</span>
+            <span class="edit-btn">✏️</span>
+        `;
+
+        addMensage.appendChild(loadTaks);
+    }
+}
+
+
+
+localLoad()
+
+
 
 
